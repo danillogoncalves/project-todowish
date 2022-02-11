54d0b241db3f31df60d5e84bfed85040425ac5eb
danillo <danillogonvalves001@gmail.com>
2022-02-11 19:00:08 -0300

    Tentando arrumar o createNewTask.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import getTasks from './helpers/getTasks.js';
 import getProjects from './helpers/getProjects.js';
+import createNewTask from '/helpers/createNewTask.js';
 
 // window.onload para testar os imports
 window.onload = async () => {
@@ -7,4 +8,5 @@ window.onload = async () => {
   console.log(projects);
   const tasks = await getTasks('934509d71c8b03ad32cc2d84eed7585ee2991228');
   console.log(tasks);
+  await createNewTask('a5a0ce723d7453794af29a0850bcde7ffbde7aa7', 'Teste 1', await getProjects('a5a0ce723d7453794af29a0850bcde7ffbde7aa7'))
 };
