6ce081e012636558d26ef35dd9bc2d0b60cb1237
SheilaNS <shei.nsantos@gmail.com>
2022-02-11 18:39:33 -0300

    Função deleteTask.js feita

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 import getTasks from './helpers/getTasks.js';
 import getProjects from './helpers/getProjects.js';
+import deleteTasks from './helpers/deleteTask.js';
 
 // window.onload para testar os imports
 window.onload = async () => {
   const projects = await getProjects('934509d71c8b03ad32cc2d84eed7585ee2991228');
   console.log(projects);
+  const deleteTask = await deleteTasks('5587639212', '934509d71c8b03ad32cc2d84eed7585ee2991228');
+  console.log(deleteTask);
   const tasks = await getTasks('934509d71c8b03ad32cc2d84eed7585ee2991228');
   console.log(tasks);
 };
