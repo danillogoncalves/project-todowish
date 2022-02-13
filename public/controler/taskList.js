19c2d443457cd26e34d55b7d892c3172d456b44f
danillo <danillogonvalves001@gmail.com>
2022-02-13 13:55:09 -0300

    Colocando o filter no tasklist.

--- public/controler/taskList.js
+++ public/controler/taskList.js
@@ -1,7 +1,7 @@
 import getTasks from '../helpers/getTasks.js';
 import getOneTask from '../helpers/getOneTask.js';
 
-const tokenDanillo = 'a5a0ce723d7453794af29a0850bcde7ffbde7aa7';
+const tokenDanillo = 'c6de81bf5190fe4984d9817473cebac3cbfae44a';
 const tokenSheila = '934509d71c8b03ad32cc2d84eed7585ee2991228';
 
 const setId = (id) => {
@@ -11,7 +11,8 @@ const setId = (id) => {
 
 getTasks(tokenSheila).then((response) => {
   const ulTask = document.querySelector('.tasks');
-  response.forEach(({ content, id }) => {
+  response.filter((label) => label.label_ids.length === 0)
+  .forEach(({ content, id }) => {
     const li = document.createElement('li');
     li.innerHTML = content;
     li.addEventListener('click', () => setId(id));
