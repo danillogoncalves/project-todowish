2b3eb3cb36e6f67f8c71c36e29d9de45302952a9
Erik-EFL <erik.lima.f@outlook.com>
2022-02-14 00:39:43 -0300

    Coisas de programador

--- public/controllers/thingCap.js
+++ public/controllers/thingCap.js
@@ -2,8 +2,9 @@ import createNewTask from '../helpers/createNewTask.js';
 
 const tokenDanillo = 'c6de81bf5190fe4984d9817473cebac3cbfae44a';
 const tokenSheila = '934509d71c8b03ad32cc2d84eed7585ee2991228';
+const tokenErik = '1f6d65f42bdc3c054ef50c71fbd8deeec3095e73';
 
 const button = document.querySelector('.btn-capture');
 const input = document.querySelector('#capture')
 
-button.addEventListener('click', async () => await createNewTask(tokenDanillo, input.value));
+button.addEventListener('click', async () => await createNewTask(tokenErik, input.value));
