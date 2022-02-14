b5a3d8bc6aabfbaa75dc1cd2fa7426a31f441240
danillo <danillogonvalves001@gmail.com>
2022-02-13 22:55:48 -0300

    Tentando resolver o problema como now.js.

--- public/controllers/not.js
+++ public/controllers/not.js
@@ -6,26 +6,14 @@ import deleteTask from '../helpers/deleteTask.js';
 const tokenDanillo = 'c6de81bf5190fe4984d9817473cebac3cbfae44a';
 const tokenSheila = '934509d71c8b03ad32cc2d84eed7585ee2991228';
 
-const taskUl = document.querySelector('.act-task');
-const newLi = localStorage.getItem('task');
-taskUl.innerHTML = newLi;
-
-
 const buttonTrash = document.querySelector('.trash');
 const buttonIncubate = document.querySelector('.incubar');
 const buttonReference = document.querySelector('.referencia');
-
-const labels = getLabels(tokenDanillo);
-labels.then(listsLabel => console.log(listsLabel));
 
 const labelTask = async (event) => {
   const labels = await getLabels(tokenDanillo);
   const { id } = labels.find((label) => event.target.className === label.name);
   const task = document.querySelector('.act-task').firstChild;
-  console.log();
-  console.log(task.innerText);
-  console.log(task.id);
-  console.log(id);
   updateTaskLabel(tokenDanillo, task.innerText, task.id, id);
   location.href = '/home';
 };
