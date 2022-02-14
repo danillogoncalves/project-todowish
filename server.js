8589b8dfede26f210957edff8c5fb16b4d49d8b3
Erik-EFL <erik.lima.f@outlook.com>
2022-02-14 02:33:30 -0300

    Adicionado browserSync ao server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
