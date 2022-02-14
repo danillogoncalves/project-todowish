d5db13ee2634c61188a5c5a6e4b46c820fbf3f9c
Erik-EFL <erik.lima.f@outlook.com>
2022-02-14 00:00:35 -0300

    Adicionando a Felicidade

--- public/controllers/now.js
+++ public/controllers/now.js
@@ -5,7 +5,7 @@ const buttonConcluded = document.querySelector('.concluded');
 const tokenDanillo = 'c6de81bf5190fe4984d9817473cebac3cbfae44a';
 const tokenSheila = '934509d71c8b03ad32cc2d84eed7585ee2991228';
 
-const finishTask = () => {
+const finishTask = async () => {
   const task = document.querySelector('.act-task').firstChild;
   await deleteTask(task.id, tokenDanillo);
   location.href = '/home';
